ef193b50d4a8d72729e6aaa075b09b5c0b97a644
Geriano <gerznewbie@gmail.com>
2022-11-25 15:23:29 +0700

    adding flash message

--- src/Layouts/DashboardLayout.tsx
+++ src/Layouts/DashboardLayout.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAppSelector } from "../hooks";
+import classNames from "classnames";
 import Main from "./DashboardLayout/Main";
 import Sidebar from "./DashboardLayout/Sidebar";
 
 export default function DashboardLayout() {
   const { authenticated } = useAppSelector(state => state.auth)
+  const flashes = useAppSelector(state => state.flash.value)
   const location = useLocation()
 
   if (!authenticated) {
@@ -12,12 +14,29 @@ export default function DashboardLayout() {
   }
 
   return (
-    <div className="relative font-sans bg-gray-200 text-gray-700 flex text-sm ">
-      <Sidebar />
+    <>
+      <div className="relative font-sans bg-gray-200 text-gray-700 flex text-sm ">
+        <Sidebar />
 
-      <Main>
-        <Outlet />
-      </Main>
-    </div>
+        <Main>
+          <Outlet />
+        </Main>
+      </div>
+
+      {flashes.filter(flash => flash).length > 0 && (
+        <div className="fixed top-0 right-0 w-full max-w-xs h-fit flex flex-col space-y-2 p-4 z-20">
+          { flashes.filter(flash => flash).map((flash, i) => (
+            <div key={i} className={classNames("bg-white border-l-8 px-4 py-2 rounded-md shadow", {
+              'border-success-0': flash!.type === 'success',
+              'border-danger-0': flash!.type === 'error',
+              'border-info-0': flash!.type === 'info',
+              'border-warning-0': flash!.type === 'warning',
+            })}>
+              {flash!.message}
+            </div>
+          )) }
+        </div>
+      )}
+    </>
   )
 }
